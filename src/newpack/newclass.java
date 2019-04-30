8ed0272a1f35e727e33fdb1b404c478a2c056080
ARUV2E11238 <ARUV2E11238@V2IT03.vee2office.com>
2019-04-30 17:11:30 +0530

    arun kumar commited

--- src/newpack/newclass.java
+++ src/newpack/newclass.java
@@ -6,6 +6,7 @@ public void meth0() {
 	System.out.println("Remote");
 	System.out.println("Remote");
 	System.out.println("Remote");
+	System.out.println("Remote");
 	// TODO Auto-generated method stub
 
 }
